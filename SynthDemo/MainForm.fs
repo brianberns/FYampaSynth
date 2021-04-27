f88e009b3a9ada6a679daa761aa0e301e0f9a5cb
Brian Berns <brianberns@gmail.com>
2021-04-27 11:56:32 -0400

    Added gain control.

--- SynthDemo/MainForm.fs
+++ SynthDemo/MainForm.fs
@@ -17,31 +17,65 @@ type MainForm() as this =
     inherit Form(
         Text = "Moog Demo",
         Size = Size(800, 400),
+        FormBorderStyle = FormBorderStyle.Fixed3D,
         StartPosition = FormStartPosition.CenterScreen)
 
-    let engine = new AudioEngine()
+    let padding = 10
 
     let trackNote =
+
+        let label =
+            new Label(
+                Text = "Note",
+                Location = Point(padding, padding),
+                AutoSize = true)
+                |> Control.addTo this
+
         new TrackBar(
-            Text = "Note",
             Minimum = 1,
             Maximum = 88,
             Value = 49,
             TickFrequency = 8,
             SmallChange = 1,
             LargeChange = 8,
-            Size = Size(this.ClientSize.Width, 50),
-            Location = Point(0, 100))
+            Size = Size(this.ClientSize.Width - label.Width - 3 * padding, 0),
+            Location = Point(label.Width + padding, label.Location.Y))
             |> Control.addTo this
 
     let getNoteFrequency note =
         440.0 * 2.0 ** ((1.0/12.0) * (float note - 49.0))
 
-    let makeSynth freq =
+    let trackVolume =
+
+        let label =
+            new Label(
+                Text = "Volume",
+                Location =
+                    Point(
+                        padding,
+                        trackNote.Location.Y + trackNote.Height + padding),
+                AutoSize = true)
+                |> Control.addTo this
+
+        new TrackBar(
+            Minimum = 0,
+            Maximum = 10,
+            Value = 2,
+            TickFrequency = 1,
+            SmallChange = 1,
+            LargeChange = 1,
+            Size = Size(this.ClientSize.Width - label.Width - 3 * padding, 0),
+            Location = Point(label.Width + padding, label.Location.Y))
+            |> Control.addTo this
+
+    let getGain volume =
+        float volume / 100.0
+
+    let makeSynth freq gain =
         let cv = Synth.oscSine 1.0
         let sawtooth = Synth.oscSawtooth freq
         (sawtooth &&& cv) >>> Synth.moogVcf 44100.0 (4.0 * freq) 0.5
-            >>^ (*) 0.02
+            >>^ (*) gain
             |> Synth
 
     let btnPlay =
@@ -53,12 +87,16 @@ type MainForm() as this =
             TextAlign = ContentAlignment.MiddleCenter)
             |> Control.addTo this
 
+    let engine = new AudioEngine()
+
     let onParamChanged _ =
         engine.RemoveAllInputs()
         if btnPlay.Checked then
             let freq = getNoteFrequency trackNote.Value
-            engine.AddInput(makeSynth freq)
+            let gain = getGain trackVolume.Value
+            engine.AddInput(makeSynth freq gain)
 
     do
         btnPlay.CheckedChanged.Add(onParamChanged)
         trackNote.ValueChanged.Add(onParamChanged)
+        trackVolume.ValueChanged.Add(onParamChanged)
