24d2f094921fcfabb396f0aa7c91816ea80f25ce
Brian Berns <brianberns@gmail.com>
2021-04-28 11:18:55 -0400

    Added Event type.

---
+++ FYampaSynth/Event.fs
@@ -0,0 +1,21 @@
+﻿namespace FYampaSynth
+
+type Event<'a> =
+    | Evt of 'a
+    | NoEvt
+
+module Event =
+
+    let tag evt value =
+        match evt with
+            | Evt _ -> Evt value
+            | NoEvt -> NoEvt
+
+    let rec switch (SF tf) f =
+        SF (fun dt a ->
+            let sf', (b, evt) = tf dt a
+            let sf'' =
+                match evt with
+                    | Evt c -> f c
+                    | NoEvt -> switch sf' f
+            sf'', b)
