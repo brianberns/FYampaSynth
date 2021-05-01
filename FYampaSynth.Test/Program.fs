dc558cb54bfea998d4c36c74291f0829c1e8c110
Brian Berns <brianberns@gmail.com>
2021-05-01 01:13:31 -0400

    Work on readme.

--- FYampaSynth.Test/Program.fs
+++ FYampaSynth.Test/Program.fs
@@ -1,1 +1,19 @@
-module Program = let [<EntryPoint>] main _ = 0
+namespace FYampaSynth.Test
+
+open System
+open FYampaSynth
+open Arrow
+
+module Program =
+
+    [<EntryPoint>]
+    let main _ =
+        use engine = new AudioEngine()
+        (Synth.oscSawtooth 440.00
+            &&& Synth.oscSine 1.0)        // note and oscillation
+            >>> Synth.moogVcf 880.0 0.5   // filter frequencies above 880 hz
+            >>^ (*) 0.05
+            |> Synth
+            |> engine.AddInput
+        Console.ReadLine() |> ignore
+        0
